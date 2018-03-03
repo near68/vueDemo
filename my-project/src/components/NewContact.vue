6b07f878b51ced90679d2bae37b9491897a779cd
near68 <near0608@163.com>
2018-03-03 23:13:39 +0800

    update

--- my-project/src/components/NewContact.vue
+++ my-project/src/components/NewContact.vue
@@ -8,6 +8,30 @@
     <el-select v-model="info.sex" placeholder="请选择">
       <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
     </el-select>
+    <template>
+      <el-table :data="tableData">
+        <el-table-column label="姓名" align="left">
+          <template slot-scope="scope">
+          <span>{{ scope.row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="年龄"  align="left">
+          <template slot-scope="scope">
+            <div slot="reference" class="name-wrapper">{{ scope.row.age }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="性别" align="left">
+          <template slot-scope="scope">
+              <div slot="reference" class="name-wrapper">{{ scope.row.sex }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="left">
+          <template slot-scope="scope">
+            <el-button size="mini"  type="danger" @click="Delete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </template>
   </el-row>
 </template>
 <script>
@@ -22,6 +46,10 @@ export default {
       },
       options: [
         '女','男','保密'
+      ],
+      tableData:[
+        {name: 'Leo', age: 12, sex: 'men'},
+        {name: 'Lei', age: 112, sex: 'men'}
       ]
     }
   }
