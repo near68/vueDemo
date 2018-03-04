b1d08cf8fa3f83edeac4363ac6b5da184009c5f9
near68 <near0608@163.com>
2018-03-04 14:37:47 +0800

    over

--- my-project/src/components/NewContact.vue
+++ my-project/src/components/NewContact.vue
@@ -1,40 +1,32 @@
 <template>
   <el-row>
-    姓名：{{info.name}}
+    <el-col :xs="24" :sm="18" :md="14" :lg="10" class="col">
+    <label>姓名：</label>  
     <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
-    年龄：{{info.age}}
+    <label>年龄：</label>
     <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
-    性别：{{info.sex}}
+    <label>性别：</label>
     <el-select v-model="info.sex" placeholder="请选择">
       <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
     </el-select>
+    <el-button  class="btn-auto" @click="create" type="success">创建</el-button>
     <template>
-      <el-table :data="tableData">
-        <el-table-column label="姓名" align="left">
-          <template slot-scope="scope">
-          <span>{{ scope.row.name }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column label="年龄"  align="left">
-          <template slot-scope="scope">
-            <div slot="reference" class="name-wrapper">{{ scope.row.age }}</div>
-          </template>
-        </el-table-column>
-        <el-table-column label="性别" align="left">
-          <template slot-scope="scope">
-              <div slot="reference" class="name-wrapper">{{ scope.row.sex }}</div>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" align="left">
-          <template slot-scope="scope">
-            <el-button size="mini"  type="danger" @click="Delete(scope.$index, scope.row)">删除</el-button>
+      <el-table :data="tabledata" align="left">
+        <el-table-column prop="name" label="姓名"></el-table-column>
+        <el-table-column prop="age" label="年龄"></el-table-column>
+        <el-table-column prop="sex" label="性别"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="a">
+            <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </template>
+    </el-col>
   </el-row>
 </template>
 <script>
+import Storage from '../store/store'
 export default {
   name: "NewContact",
   data(){
@@ -47,14 +39,39 @@ export default {
       options: [
         '女','男','保密'
       ],
-      tableData:[
-        {name: 'Leo', age: 12, sex: 'men'},
-        {name: 'Lei', age: 112, sex: 'men'}
-      ]
+      tabledata: Storage.fetch()
+    }
+  },
+  methods: {
+    create(){
+      this.tabledata.push(this.info)
+      this.info =  {name: '', age: null, sex: ''}
+    },
+    del(index){
+      this.tabledata.splice(index,1)
+    }
+  },
+  watch:{
+    tabledata:{
+      handler(items){Storage.save(items)},
+      deep: true
     }
   }
 }
 </script>
 <style>
-
+  .col{
+      float: none;
+      margin: 0 auto;
+  }
+  .el-input{
+    padding-bottom: 5px;
+  }
+  .el-select {
+      display: block;
+  }
+  .btn-auto{
+      width: 100%;
+      margin-top: 12px;
+  }
 </style>
